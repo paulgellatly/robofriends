827d686d256aae059291ab03ba51ada3b43e3537
Paul G <paulgellatly@icloud.com>
2020-11-15 18:48:05 +0000

    built card file and styled it

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './Hello';
+import Card from './Card';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
+import { robots } from './robots';
 
 ReactDOM.render(
     <React.StrictMode>
-        <Hello greeting={'Hello React Ninja'} />
+        <div>
+            <Card
+                id={robots[0].id}
+                name={robots[0].name}
+                username={robots[0].username}
+                email={robots[0].email}
+            />
+            <Card
+                id={robots[1].id}
+                name={robots[1].name}
+                username={robots[1].username}
+                email={robots[1].email}
+            />
+            <Card
+                id={robots[2].id}
+                name={robots[2].name}
+                username={robots[2].username}
+                email={robots[2].email}
+            />
+        </div>
     </React.StrictMode>,
     document.getElementById('root')
 );
